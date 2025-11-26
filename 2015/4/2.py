7f26579ae43ca856e61578a7afd152cd98d8acd8
Alejandro Sánchez Cano <a.sanchezcano@uva.nl>
2025-11-26 16:23:23 +0100

    tweak importing

--- 2015/4/2.py
+++ 2015/4/2.py
@@ -4,13 +4,13 @@ Brute-force search.
 '''
 
 import hashlib
+import itertools
 from tqdm import tqdm
-from itertools import count
 
 with open(__file__.rsplit("/", 1)[0] + "/input.txt") as f:
     secret_key = f.read()
 
-for idx in tqdm(count()):
+for idx in tqdm(itertools.count()):
     tohash = secret_key + str(idx)
     md5 = hashlib.md5(tohash.encode())
     hexadecimal = md5.hexdigest()
