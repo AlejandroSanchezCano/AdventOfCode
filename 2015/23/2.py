f32291901fc9deb7db10506fef9cbb21c3559f83
Alejandro Sánchez Cano <a.sanchezcano@uva.nl>
2025-12-03 12:36:21 +0100

    2015 23

---
+++ 2015/23/2.py
@@ -0,0 +1,56 @@
+'''
+--- Day 23.2: Opening the Turing Lock ---
+Same as part 1.
+'''
+
+with open(__file__.rsplit("/", 1)[0] + "/input.txt") as f:
+    lines = f.read().splitlines()
+
+class Instruction:
+    
+    def __init__(self, instruction: str):
+        self.instruction = instruction
+        self.command = instruction[:3]
+        if self.command == 'jmp':
+            self.offset = int(instruction.split()[1])
+        elif self.command in ('jie', 'jio'):
+            self.register = instruction[4]
+            self.offset = int(instruction.split(', ')[-1])
+        else:
+            self.register = instruction[4]
+            self.offset = 1
+
+    def __repr__(self):
+        return str(self.instruction)
+
+    def execute(self):
+        if self.command == 'hlf':
+            registers[self.register] //= 2
+        elif self.command == 'tpl':
+            registers[self.register] *= 3
+        elif self.command == 'inc':
+            registers[self.register] += 1
+        elif self.command == 'jmp':
+            return self.offset
+        elif self.command == 'jie':
+            if registers[self.register] % 2 == 0:
+                return self.offset
+        elif self.command == 'jio':
+            if registers[self.register] == 1:
+                return self.offset
+        return 1
+
+registers = {
+    'a': 1,
+    'b': 0,
+    }
+
+idx = 0
+instructions = [Instruction(instruction) for instruction in lines]
+while True:
+    offset = instructions[idx].execute()
+    idx += offset
+    if idx >= len(instructions): 
+        break
+
+print(registers['b'])
